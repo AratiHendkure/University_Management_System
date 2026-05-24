2cba44e918d57cf417f36718131fd4eca6aa4d00
Arati Hendkure <aratihendkure60@gmail.com>
2026-05-24 14:01:22 +0000

    api is working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res)=>{
     console.log("API is running")
-    res.send("API is running")
+    res.send("API is running and i am happy")
 })
 
 app.listen(4000)
